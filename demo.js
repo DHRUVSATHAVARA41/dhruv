7bffef3bb7123bfba12b011b3bfe11e4b0edca92
DHRUVSATHAVARA41 <137283658+DHRUVSATHAVARA41@users.noreply.github.com>
2023-06-27 10:02:52 -0400

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,14 @@
+function green(){
+ let a = alert("fill body with background color green");
+ document.body.style.background="green";
+}
+
+function red(){
+ let a = confirm("fill body with background color red");
+ document.body.style.background="red";
+}
+
+function green(){
+ let a = alert("fill body with background color?");
+ document.body.style.background="blue";
+}
